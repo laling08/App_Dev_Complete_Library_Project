c77db477d4289995ffe7a93acacb2d46688eed9a
Ishilia <ishiliagilcedeslabrador@gmail.com>
2024-11-28 11:33:31 -0500

    Deleted Adapters and adapter Interface

--- CompleteLibrary_Project/obj/Debug/net8.0-windows/CompleteLibrary_Project.AssemblyInfo.cs
+++ CompleteLibrary_Project/obj/Debug/net8.0-windows/CompleteLibrary_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompleteLibrary_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+012f8597bf70adf9381deaee412736f5e39ec06a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8294e1ff61144f85a23e6473e08a2758e299046")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompleteLibrary_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompleteLibrary_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
